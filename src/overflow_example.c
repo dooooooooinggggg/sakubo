a489bfbc536b4d10ff0bd4b677de523355be9d13
niconegoto <niconegoto@yahoo.co.jp>
2019-12-15 22:52:09 +0900

    Add auth_overflow

--- src/overflow_example.c
+++ src/overflow_example.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
 	printf("[BEFORE] buffer_one is at %p and contains \'%s\'\n", buffer_one, buffer_one);
 	printf("[BEFORE] value is at %p and is %d (0x%08x)\n", &value, value, value);
 
-	printf("\n[STRCPY] copying %d bytes into buffer_two\n\n",  strlen(argv[1]));
+	printf("\n[STRCPY] copying %lu bytes into buffer_two\n\n",  strlen(argv[1]));
 	strcpy(buffer_two, argv[1]); /* copy first argument into buffer_two */
 
 	printf("[AFTER] buffer_two is at %p and contains \'%s\'\n", buffer_two, buffer_two);

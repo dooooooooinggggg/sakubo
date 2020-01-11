c7379bdc0da4fb9a53e3341982a857f9e61d03fa
dooooooooinggggg <dooooooooinggggg@gmail.com>
2020-01-11 15:39:42 +0900

    fix

---
+++ src/chapter3/byte_size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 0;
+    float b = 0;
+    unsigned int c = 0;
+    long d = 0;
+    printf("%d\n", sizeof(a));
+    printf("%d\n", sizeof(b));
+    printf("%d\n", sizeof(c));
+    printf("%d\n", sizeof(d));
+    return 0;
+}

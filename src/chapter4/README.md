788b6e88c9672d55d2b846b6329ec5c0717218c6
dooooooooinggggg <dooooooooinggggg@gmail.com>
2020-02-08 15:22:36 +0900

    fmt and README

---
+++ src/chapter4/README.md
@@ -0,0 +1,34 @@
+# Chapter4
+
+## tinyweb
+
+### Server
+
+```sh
+make
+./tinyweb
+```
+
+### Client
+
+Access to [http://127.0.0.1/](http://127.0.0.1/)
+
+## simple-server
+
+### Server
+
+```sh
+make
+./simple-server
+```
+
+### Client
+
+```sh
+telnet 127.0.0.1 7890
+# Trying 127.0.0.1...
+# Connected to localhost.
+# Escape character is '^]'.
+# Hello World!
+# 好きな文字列を入力してみる
+```
--- src/chapter4/simple-server.c
+++ src/chapter4/simple-server.c
@@ -6,13 +6,14 @@
 #include <arpa/inet.h>
 #include "hacking.h"
 
-#define PORT 7890	// the port users will be connecting to
+#define PORT 7890 // the port users will be connecting to
 
-int main(void) {
-	int sockfd, new_sockfd;  // listen on sock_fd, new connection on new_fd
-	struct sockaddr_in host_addr, client_addr;	// my address information
+int main(void)
+{
+	int sockfd, new_sockfd;					   // listen on sock_fd, new connection on new_fd
+	struct sockaddr_in host_addr, client_addr; // my address information
 	socklen_t sin_size;
-	int recv_length=1, yes=1;
+	int recv_length = 1, yes = 1;
 	char buffer[1024];
 
 	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1)
@@ -20,9 +21,9 @@ int main(void) {
 
 	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
 		fatal("setting socket option SO_REUSEADDR");
-	
-	host_addr.sin_family = AF_INET;		 // host byte order
-	host_addr.sin_port = htons(PORT);	 // short, network byte order
+
+	host_addr.sin_family = AF_INET;			// host byte order
+	host_addr.sin_port = htons(PORT);		// short, network byte order
 	host_addr.sin_addr.s_addr = INADDR_ANY; // automatically fill with my IP
 	memset(&(host_addr.sin_zero), '\0', 8); // zero the rest of the struct
 
@@ -32,15 +33,17 @@ int main(void) {
 	if (listen(sockfd, 5) == -1)
 		fatal("listening on socket");
 
-	while(1) {    // Accept loop
+	while (1)
+	{ // Accept loop
 		sin_size = sizeof(struct sockaddr_in);
 		new_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
-		if(new_sockfd == -1)
+		if (new_sockfd == -1)
 			fatal("accepting connection");
-		printf("server: got connection from %s port %d\n",inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+		printf("server: got connection from %s port %d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 		send(new_sockfd, "Hello World!\n", 13, 0);
 		recv_length = recv(new_sockfd, &buffer, 1024, 0);
-		while(recv_length > 0) {
+		while (recv_length > 0)
+		{
 			printf("RECV: %d bytes\n", recv_length);
 			dump(buffer, recv_length);
 			recv_length = recv(new_sockfd, &buffer, 1024, 0);
